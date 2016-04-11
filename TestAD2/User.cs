ec694a7dd4becca4b6bd3d1c34bd1608235ee19d
Lê Ngọc Tuấn <tuanlengoc@hotmail.com>
2016-04-11 19:13:15 +0700

    frist commit

---
+++ TestAD2/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAD2
+{
+    class User
+    {
+        /// <summary>
+        /// Các giá trị lưu trong lớp User tương ứng
+        /// - SAMAccountName: Tên đăng nhập của User
+        /// - commonName: tên đầy đủ của User
+        /// - ou: OU của User
+        /// </summary>
+        public string SAMAccountName { get; set; }
+        public string commonName { get; set; }
+        public string ou { get; set; }
+    }
+}
